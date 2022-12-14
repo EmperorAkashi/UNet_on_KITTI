dd514975a04f9ecf81bbba32bfe05c4ce330482d
EmperorAkashi <linchen4869@ucla.edu>
2022-12-13 23:36:57 -0800

    change dataset with os processed path

--- UNet_on_KITTI/dataset.py
+++ UNet_on_KITTI/dataset.py
@@ -2,21 +2,27 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader, sampler
 import os
 import cv2 as cv
+from file_utils import read_from_folder
+
 
 class kitti_dataset(Dataset):
-    "Dataset to load semantic data from Kitti"
+    """
+    Dataset to load semantic data from Kitti
+    the dir is the top folder of dataset; the read_from_folder 
+    will outputs a dict with {folder: [list file names]}
+    """
 
     def __init__(self, dir, transform = None):
         self.transform = transform
-        self.dir = image_dir
-        self.df = dataframe #need check the def of dataframe of kitti
+        self.dir = dir
+        self.df =  read_from_folder(dir)#
 
     def __len__(self):
-        return len(self.df)
+        return len(self.df[0])
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.img_dir, '00000'+str(index)+'_10.png')
-        seg_path = os.path.join(self.seg_dir, '00000'+str(index)+'_10.png')
+        img_path = os.path.join(self.dir, 'training', self.df['training'][index])
+        seg_path = os.path.join(self.dir, 'semantic', self.df['semantic'][index])
         image = cv.imread(img_path)
         segment = cv.imread(seg_path)
         return image, segment
