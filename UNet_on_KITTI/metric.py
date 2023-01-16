2796bff2ec0a539e3e73304ce9d14a4f56cf5d2d
EmperorAkashi <linchen4869@ucla.edu>
2023-01-15 21:32:52 -0800

    start f1 by dice score of 1 class

--- UNet_on_KITTI/metric.py
+++ UNet_on_KITTI/metric.py
@@ -2,6 +2,15 @@ import numpy as np
 import torch
 import sklearn as sk
 
-
-def mIoU(predict, mask):
-    return sk.metrics.jaccard_score(mask, predict)
+def dice_score_one(inputs, targets, smooth=1e-5):      
+    """
+    dice loss for the binary case (one category)
+    """
+    #flatten label and prediction tensors
+    inputs = inputs.view(-1)
+    targets = targets.view(-1)
+        
+    intersection = (inputs * targets).sum()                            
+    dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+    return dice
