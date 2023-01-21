317968374b7e3aa7403b850dcb80c9b7243ddaa2
EmperorAkashi <linchen4869@ucla.edu>
2023-01-21 14:52:05 -0800

    add super

--- UNet_on_KITTI/feed_forward.py
+++ UNet_on_KITTI/feed_forward.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 class feed_forward:
     def __init__(self, in_ch, out_ch, hidden_layer):
-        super().__init__()
+        super(feed_forward).__init__()
         self.l1 = nn.Linear(in_ch, hidden_layer)
         self.relu = nn.ReLU()
         self.l2 = nn.Linear(hidden_layer, out_ch)
-    def forward(self,x):
+    def forward(self,x) -> torch.Tensor:
         output = self.l1(x) 
         output = self.relu(output)
         output = self.l2(output)
