cbf5305e837d63450923c8f2aeaa685db328c840
EmperorAkashi <linchen4869@ucla.edu>
2023-01-25 17:13:13 -0500

    change ff

--- src/feed_forward.py
+++ src/feed_forward.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 "construct Double Convolution block for UNet"
 class feed_forward(torch.nn.Module):
     def __init__(self, in_ch, out_ch, kernel):
-        super(DoubleConv, self).__init__()
+        super().__init__()
         self.conv = nn.Sequential( 
             nn.Conv2d(in_ch, out_ch, kernel_size = kernel, stride=1, padding=1),
             nn.BatchNorm2d(out_ch),
