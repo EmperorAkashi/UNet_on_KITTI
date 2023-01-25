08b3c10fc7ec3e87fc2489169bb0e7977c2c07c7
EmperorAkashi <linchen4869@ucla.edu>
2023-01-25 17:30:46 -0500

    change ff

--- src/feed_forward.py
+++ src/feed_forward.py
@@ -5,15 +5,16 @@ import torch.nn as nn
 class feed_forward(torch.nn.Module):
     def __init__(self, in_ch, out_ch, kernel):
         super().__init__()
-        self.conv = nn.Sequential( 
-            nn.Conv2d(in_ch, out_ch, kernel_size = kernel),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, kernel_size = kernel),
-            nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True)
-         )
+        # self.conv = nn.Sequential( 
+        #     nn.Conv2d(in_ch, out_ch, kernel_size = kernel),
+        #     nn.BatchNorm2d(out_ch),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(out_ch, out_ch, kernel_size = kernel),
+        #     nn.BatchNorm2d(out_ch),
+        #     nn.ReLU(inplace=True)
+        #  )
+        self.relu = nn.ReLU()
     def forward(self, x) -> torch.Tensor:
-        x = self.conv(x)
+        x = self.relu(x)
         return x
 
